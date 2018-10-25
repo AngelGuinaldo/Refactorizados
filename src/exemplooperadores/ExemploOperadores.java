d046bdc076f229804ee0e2dd6dcae2c60046e208
aguinaldogonzalez <aguinaldogonzalez@nautilus4.danielcastelao.org>
2018-10-25 08:45:54 +0200

    Despues de Factorizar

--- src/exemplooperadores/ExemploOperadores.java
+++ src/exemplooperadores/ExemploOperadores.java
@@ -1,41 +1,33 @@
-
 package exemplooperadores;
-
 
 public class ExemploOperadores {
 
-   
     public static void main(String[] args) {
-        
-        int num1=0 ,num2= 4;
-        System.out.println("num1= "+num1 +"\nnum2 ="+ num2);
-        num1=num2++;        
-        System.out.println("(num1=num2++) "+num1 +"\nnum2 ="+ num2);
-        num1=++num2;
-        System.out.println("(num1=++num2) \n"+num1 +"=num1 "+"\nnum2 ="+ num2);
-        
-        num1=3;num2= 4;
+
+        int num1 = 0, num2 = 4;
+        System.out.println("num1= " + num1 + "\nnum2 =" + num2);
+        num1 = num2++;
+        System.out.println("(num1=num2++) " + num1 + "\nnum2 =" + num2);
+        num1 = ++num2;
+        System.out.println("(num1=++num2) \n" + num1 + "=num1 " + "\nnum2 =" + num2);
+
+        num1 = 3;
+        num2 = 4;
         int suma;
-        suma=num1+num2;
-        System.out.println("suma = "+suma);
+        suma = num1 + num2;
+        System.out.println("suma = " + suma);
         //Tamen pode facerse
-        System.out.println("suma = "+(num1+num2));//sale igual, con esto se puede ahorrar en memoria y la sentencia
-        
-    //Casting
-        int enteiro=6;
-        float decimal=2.3f;
+        System.out.println("suma = " + (num1 + num2));//sale igual, con esto se puede ahorrar en memoria y la sentencia
+
+        //Casting
+        int enteiro = 6;
+        float decimal = 2.3f;
         //conversion automática
-        decimal=enteiro;
+        decimal = enteiro;
         System.out.println(decimal);
         //Casting
-        enteiro=(int) decimal; //De maior a menor precision tenemos que hacer cast
-        
-        
-        
-        
-        
-        
-    
+        enteiro = (int) decimal; //De maior a menor precision tenemos que hacer cast
+
     }
-    
+
 }
